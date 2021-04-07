77225d67a9a6530a62bb8de4066d895e6ff5851c
Ivan Alexandrov <ivan@alexandrov.online>
2021-04-08 00:14:53 +0200

    Update tinyAgo.js

--- tinyAgo.js
+++ tinyAgo.js
@@ -1,11 +1,14 @@
 function ago(val) {
     val = 0 | (Date.now() - val) / 1000;
-    var unit, length = { second: 60, minute: 60, hour: 24, day: 7, week: 4.35,
-        month: 12, year: 10000 }, result;
+    var unit, length = { секунд: 60, минут: 60, час: 24, ден: 7, недел: 4.35,
+        месяц: 12, год: 10000 }, result;
+    var decl = { секунд: ["а","ы",""], минут: ["а","ы",""], час: ["","а","ов"], ден: ["день","дня","дней"], недел: ["я","и","ь"],
+        месяц: ["","а","ев"], год: ["год","года","лет"]};
+    var decf = ["ден", "год"];
  
     for (unit in length) {
         result = val % length[unit];
         if (!(val = 0 | val / length[unit]))
-            return result + ' ' + (result-1 ? unit + 's' : unit);
+            return result + ' ' + (decf.indexOf(unit)+1 ? "" : unit) + decl[unit][result%10>4 ? 2 : result%10-1 ? 1 : 0];
     }
 }
